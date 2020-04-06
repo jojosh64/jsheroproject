b700cddff036dc9f5a6bb44e1252bfb42fa8a6aa
Josh Brummund <jojosh64@hotmail.com>
2020-04-06 16:44:43 -0600

    code for question 66, returning the greatest common divisor of 2 numbers

---
+++ Q.66GreatestCommonDenominator.js
@@ -0,0 +1,18 @@
+const gcd = (num1, num2) => {
+  let highestNum;
+  let testNum = 0;
+  let currentHighestDenominator;
+
+  if (num1 > num2) highestNum = num1;
+
+  if (num2 > num1) highestNum = num2;
+
+  if (num1 === num2) return num1;
+  while (testNum <= highestNum) {
+    if (num1 % testNum === 0 && num2 % testNum === 0) {
+      currentHighestDenominator = testNum;
+    }
+    testNum++;
+  }
+  return currentHighestDenominator;
+};
